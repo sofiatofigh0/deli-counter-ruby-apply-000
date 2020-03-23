3516b37caec24f5c66b48b4aca300a96b51b00a2
sofia tofigh <sst2140@columbia.edu>
2020-03-23 03:22:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,9 @@ end
 
 def take_a_number(katz_deli,name)
   
-   katz_deli.push(name)
-   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+   katz_deli << name
+   
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
          
   end
 end
